eb7e0c903f5f9ea0b3237b1ddd7ba313d3da744d
djowel <djowel@gmail.com>
2018-11-03 07:19:30 +0800

    moved some support stuff from q

--- include/infra/support.hpp
+++ include/infra/support.hpp
@@ -25,46 +25,85 @@ namespace cycfi
    using time_point     = std::chrono::time_point<std::chrono::steady_clock, duration>;
 
    ////////////////////////////////////////////////////////////////////////////
-   // static int types
-	////////////////////////////////////////////////////////////////////////////
-   template <typename T, T value_>
-   struct static_int
+   // Metaprogramming utilities
+   ////////////////////////////////////////////////////////////////////////////
+   template <typename T, typename... Rest>
+   struct is_arithmetic
    {
-      static constexpr T value =  value_;
+      static constexpr bool value
+         = std::is_arithmetic<T>::value && is_arithmetic<Rest...>::value;
    };
 
-   template <int i>
-   using int_ = static_int<int, i>;
+   template <typename T>
+   struct is_arithmetic<T>
+   {
+      static constexpr bool value = std::is_arithmetic<T>::value;
+   };
 
-   template <std::size_t i>
-   using uint_ = static_int<std::size_t, i>;
+   namespace detail
+   {
+      template <uint32_t bits>
+      struct int_that_fits_impl { using type = void; };
 
-   template <int8_t i>
-   using int8_ = static_int<int8_t, i>;
+      template <>
+      struct int_that_fits_impl<8> { using type = std::int8_t; };
 
-   template <uint8_t i>
-   using uint8_ = static_int<uint8_t, i>;
+      template <>
+      struct int_that_fits_impl<16> { using type = std::int16_t; };
 
-   template <int16_t i>
-   using int16_ = static_int<int16_t, i>;
+      template <>
+      struct int_that_fits_impl<32> { using type = std::int32_t; };
 
-   template <uint16_t i>
-   using uint16_ = static_int<uint16_t, i>;
+      template <>
+      struct int_that_fits_impl<64> { using type = std::int64_t; };
 
-   template <int32_t i>
-   using int32_ = static_int<int32_t, i>;
+      template <uint32_t bits>
+      struct uint_that_fits_impl { using type = void; };
 
-   template <uint32_t i>
-   using uint32_ = static_int<uint32_t, i>;
+      template <>
+      struct uint_that_fits_impl<8> { using type = std::uint8_t; };
 
-   template <int64_t i>
-   using int64_ = static_int<int64_t, i>;
+      template <>
+      struct uint_that_fits_impl<16> { using type = std::uint16_t; };
 
-   template <uint64_t i>
-   using uint64_ = static_int<uint64_t, i>;
+      template <>
+      struct uint_that_fits_impl<32> { using type = std::uint32_t; };
+
+      template <>
+      struct uint_that_fits_impl<64> { using type = uint64_t; };
+
+      constexpr std::uint32_t size_that_fits_int(uint32_t bits)
+      {
+         if (bits <= 8)
+            return 8;
+         else if (bits <= 16)
+            return 16;
+         else if (bits <= 32)
+            return 32;
+         return 0;
+      }
+   }
+
+   template <uint32_t bits>
+   struct int_that_fits
+     : detail::int_that_fits_impl<detail::size_that_fits_int(bits)>
+   {
+      static_assert(std::is_same<typename int_that_fits<bits>::type, void>::value,
+         "Error: No int type fits specified number of bits."
+      );
+   };
+
+   template <uint32_t bits>
+   struct uint_that_fits
+     : detail::uint_that_fits_impl<detail::size_that_fits_int(bits)>
+   {
+      static_assert(std::is_same<typename uint_that_fits<bits>::type, void>::value,
+         "Error: No int type fits specified number of bits."
+      );
+   };
 
    ////////////////////////////////////////////////////////////////////////////
-   // Some constants
+   // Constants
 	////////////////////////////////////////////////////////////////////////////
    template <typename T>
    struct int_traits;
@@ -72,8 +111,8 @@ namespace cycfi
    template <>
    struct int_traits<uint32_t>
    {
-      static constexpr uint32_t max = 4294967295;
-      static constexpr uint32_t min = 0;
+      static constexpr std::uint32_t max = 4294967295;
+      static constexpr std::uint32_t min = 0;
    };
 
    template <>
@@ -122,5 +161,113 @@ namespace cycfi
    {
       return int_traits<T>::min;
    }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // integer and binary functions
+   ////////////////////////////////////////////////////////////////////////////
+   constexpr int16_t promote(int8_t i)
+   {
+      return i;
+   }
+
+   constexpr uint16_t promote(uint8_t i)
+   {
+      return i;
+   }
+
+   constexpr int32_t promote(int16_t i)
+   {
+      return i;
+   }
+
+   constexpr std::uint32_t promote(uint16_t i)
+   {
+      return i;
+   }
+
+   constexpr int64_t promote(int32_t i)
+   {
+      return i;
+   }
+
+   constexpr uint64_t promote(uint32_t i)
+   {
+      return i;
+   }
+
+   constexpr float promote(float i)
+   {
+      return i;
+   }
+
+   constexpr double promote(double i)
+   {
+      return i;
+   }
+
+   template <typename T>
+   constexpr T pow2(std::size_t n)
+   {
+      return (n == 0)? T(1) : T(2) * pow2<T>(n-1);
+   }
+
+   // This is needed to force compile-time evaluation
+   template <typename T, size_t n>
+   struct static_pow2
+   {
+      constexpr static T val = pow2<T>(n);
+   };
+
+   // smallest power of 2 that fits n
+   template <typename T>
+   constexpr T smallest_pow2(T n, T m = 1)
+   {
+      return (m < n)? smallest_pow2(n, m << 1) : m;
+   }
+
+   template <typename T>
+   constexpr bool is_pow2(T n)
+   {
+      return (n & (n - 1)) == 0;
+   }
+
+   ////////////////////////////////////////////////////////////////////////////
+   // static int types
+	////////////////////////////////////////////////////////////////////////////
+   template <typename T, T value_>
+   struct static_int
+   {
+      static constexpr T value =  value_;
+   };
+
+   template <int i>
+   using int_ = static_int<int, i>;
+
+   template <std::size_t i>
+   using uint_ = static_int<std::size_t, i>;
+
+   template <int8_t i>
+   using int8_ = static_int<int8_t, i>;
+
+   template <uint8_t i>
+   using uint8_ = static_int<uint8_t, i>;
+
+   template <int16_t i>
+   using int16_ = static_int<int16_t, i>;
+
+   template <uint16_t i>
+   using uint16_ = static_int<uint16_t, i>;
+
+   template <int32_t i>
+   using int32_ = static_int<int32_t, i>;
+
+   template <uint32_t i>
+   using uint32_ = static_int<uint32_t, i>;
+
+   template <int64_t i>
+   using int64_ = static_int<int64_t, i>;
+
+   template <uint64_t i>
+   using uint64_ = static_int<uint64_t, i>;
 }
 #endif
